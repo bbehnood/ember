7eb649f4e783ca76e06f4635dcd7056defa8c293
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-07-17 12:50:55 +0330

    Define the AST nodes

---
+++ src/ast.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr<'a> {
+    Identifier(&'a [u8]),
+    Number(i64),
+
+    Binary { left: Box<Expr<'a>>, operator: BinaryOp, right: Box<Expr<'a>> },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Statement<'a> {
+    Let { name: &'a [u8], value: Expr<'a> },
+
+    Expression(Expr<'a>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Program<'a> {
+    statements: Vec<Statement<'a>>,
+}
